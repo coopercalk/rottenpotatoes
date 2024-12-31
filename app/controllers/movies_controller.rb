16e37f28a5783193a48b4ce896506782a5564544
Cooper Calk <coopercalk@gmail.com>
2024-12-31 11:49:20 +0800

    Implement sorting functionality for movies list

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,14 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    @sort_column = params[:sort] || session[:sort] || 'title'
+    @sort_direction = params[:direction] || session[:direction] || 'asc'
+  
+    # Store sort params in session
+    session[:sort] = @sort_column
+    session[:direction] = @sort_direction
+  
+    @movies = Movie.order("#{@sort_column} #{@sort_direction}")
   end
 
   # GET /movies/1 or /movies/1.json
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,14 @@
 module MoviesHelper
+  def sort_indicator(column)
+    return '' unless params[:sort] == column
+    params[:direction] == 'asc' ? '▲' : '▼'
+  end
+  
+  def next_sort_direction(column)
+    if params[:sort] != column
+      'asc'
+    else
+      params[:direction] == 'asc' ? 'desc' : 'asc'
+    end
+  end
 end
