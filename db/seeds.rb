45e201bce3222dd6eaaf5a5037a42e59fa816bcf
Cooper Calk <coopercalk@gmail.com>
2024-12-31 09:03:07 +0800

    Update seed with new movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,28 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 # Seed the RottenPotatoes DB with some movies.
-more_movies = [
-  {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+# more_movies = [
+#   {:title => 'My Neighbor Totoro', :rating => 'G',
+#     :release_date => '16-Apr-1988'},
+#   {:title => 'Green Book', :rating => 'PG-13',
+#     :release_date => '16-Nov-2018'},
+#   {:title => 'Parasite', :rating => 'R',
+#     :release_date => '30-May-2019'},
+#   {:title => 'Nomadland', :rating => 'R',
+#     :release_date => '19-Feb-2021'},
+#   {:title => 'CODA', :rating => 'PG-13',
+#     :release_date => '13-Aug-2021'}
+# ]
+
+new_movies = [
+  {:title => 'Cars', :rating => 'G',
+    :release_date => '09-Jun-2006'},
+  {:title => 'Tenet', :rating => 'PG-13',
+    :release_date => '03-Sep-2020'},
+  {:title => 'The Hunger Games', :rating => 'PG-13',
+    :release_date => '23-Mar-2012'},
 ]
 
-more_movies.each do |movie|
+new_movies.each do |movie|
   Movie.create!(movie)
 end
